0b446a3e1665b6500f19cb4d1aada855fc912858
David Patrício <david.c.patricio@gmail.com>
2024-01-25 21:23:40 +0000

    Configure static files

--- src/controllers/Student.js
+++ src/controllers/Student.js
@@ -8,7 +8,7 @@ class StudentController {
       order: [['id', 'DESC'], [Photo, 'id', 'DESC']],
       include: {
         model: Photo,
-        attributes: ['filename'],
+        attributes: ['url', 'filename'],
       },
     });
     res.json(students);
@@ -29,7 +29,7 @@ class StudentController {
         order: [['id', 'DESC'], [Photo, 'id', 'DESC']],
         include: {
           model: Photo,
-          attributes: ['filename'],
+          attributes: ['url', 'filename'],
         },
       });
 
