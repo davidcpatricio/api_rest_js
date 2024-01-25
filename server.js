e0aae7ff25fefd9a1625d5194233ba529454f976
David Patrício <david.c.patricio@gmail.com>
2024-01-25 14:45:37 +0000

    CRUD users

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ import app from './app';
 const port = 3001;
 app.listen(port, () => {
   console.log();
-  console.log(`Listening on ${port}`);
+  console.log(`Listening on port ${port}...`);
   console.log(`CTRL + Click on http://localhost:${port}`);
 });
--- src/controllers/User.js
+++ src/controllers/User.js
@@ -4,9 +4,77 @@ class UserController {
   async store(req, res) {
     try {
       const newUser = await User.create(req.body);
-      res.json(newUser);
+      return res.json(newUser);
     } catch (e) {
-      res.status(400).json({
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+      return res.json(user);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['Empty ID.'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['User does not exist.'],
+        });
+      }
+
+      const newData = await user.update(req.body);
+      return res.json(newData);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['Empty ID.'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['User does not exist.'],
+        });
+      }
+
+      await user.destroy();
+      return res.json(user);
+    } catch (e) {
+      return res.status(400).json({
         errors: e.errors.map((err) => err.message),
       });
     }
