bd32c2a9c1b925874375089451e764bee10d4554
David Patrício <david.c.patricio@gmail.com>
2024-01-29 14:17:04 +0000

    Configure CORS

--- app.js
+++ app.js
@@ -6,11 +6,28 @@ dotenv.config();
 import './src/database';
 
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+
 import homeRoutes from './src/routes/home';
 import userRoutes from './src/routes/user';
 import tokenRoutes from './src/routes/token';
 import studentRoutes from './src/routes/student';
 import photoRoutes from './src/routes/photo';
+
+const WhiteList = [
+  'http://localhost:3001',
+];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (WhiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
 class App {
   constructor() {
@@ -20,6 +37,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors());
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(express.static(resolve(__dirname, 'uploads')));
