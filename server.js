11e70286c9fc211fc6fc7bee4691acdaf2477e98
David Patrício <david.c.patricio@gmail.com>
2024-03-12 16:06:47 +0000

    Remove access to get route in users

--- server.js
+++ server.js
@@ -1,8 +1,4 @@
 import app from './app';
 
 const port = process.env.APP_PORT;
-app.listen(port, () => {
-  console.log();
-  console.log(`Listening on port ${port}...`);
-  console.log(`CTRL + Click on http://localhost:${port}`);
-});
+app.listen(port);
